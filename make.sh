857f38c58641453600c5c0500202810489a989ba
tsoganov <sergei.tsoganov@ut.ee>
2024-10-29 10:49:22 +0200

    Updated make.sh

--- make.sh
+++ make.sh
@@ -3,10 +3,16 @@
 echo "Getting started"
 
 # Bundle docs into zero-dependency HTML file
-npx @redocly/cli build-docs openapi/openapi.yaml -o index.html && \
+npx @redocly/cli build-docs openapi/openapi.yaml -o index.html
+
+# Check if index.html exists
+if [ ! -f index.html ]; then
+    echo "index.html does not exist!"
+    exit 1
+fi
 
 # Add favicon
 sed -i '' '8i\
-    <link rel="icon" type="image/png" href="docs/favicon.png"/>' index.html && \
+    <link rel="icon" type="image/png" href="docs/favicon.png"/>' index.html
 
 echo -e "\nDone!"
